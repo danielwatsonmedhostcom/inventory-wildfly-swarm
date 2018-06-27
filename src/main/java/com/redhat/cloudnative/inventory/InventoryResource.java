d10f0b177eca77ebf52cd23efe025b262c63ca6e
developer <developer@me.com>
2018-06-27 18:33:28 +0000

    inventory returns zero for non-existing product id

--- src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
+++ src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
@@ -1,9 +1,16 @@
 package com.redhat.cloudnative.inventory;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.persistence.*;
-import javax.ws.rs.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.wildfly.swarm.health.Health;
+import org.wildfly.swarm.health.HealthStatus;
+import java.util.Date;
 
 @Path("/")
 @ApplicationScoped
@@ -16,6 +23,13 @@ public class InventoryResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
         Inventory inventory = em.find(Inventory.class, itemId);
+
+        if (inventory == null) {
+            inventory = new Inventory();
+            inventory.setItemId(itemId);
+            inventory.setQuantity(0);
+        }
+
         return inventory;
     }
 }
